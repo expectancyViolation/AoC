b17c849e4722e90bf9a9ff711d6379b3f2a068a9
MTh <->
2021-12-01 09:54:01 +0100

    2021 day 1

---
+++ 2021/day01.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from util import *
+
+DAY = 1
+YEAR = 2021
+
+
+def part1(data):
+    return sum(x < y for x, y in zip(data, data[1:]))
+
+
+def part2(data):
+    return sum(x < y for x, y in zip(data, data[3:]))
+
+
+if __name__ == "__main__":
+    data = get_data(DAY, year=YEAR)
+    print(data)
+    # res = part1(data)
+    # print(res)
+    # submit(DAY, 1, res, year=YEAR)
+    res = part2(data)
+    print(res)
+    submit(DAY, 2, res, year=YEAR)
