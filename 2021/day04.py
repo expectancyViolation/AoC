585a4a9f4e7e9b7b0fd4490269699b830e8539bb
matze <>
2021-12-04 22:09:00 +0100

    2021 day 4

--- 2021/day04.py
+++ 2021/day04.py
@@ -14,23 +14,29 @@ def unmarked_grid_sum(grid):
     return sum(sum(y for y in x if y != DRAWN) for x in grid)
 
 
+def is_won(grid):
+    row_win = any(
+        all(x == DRAWN for x in grid[i]) for i in range(len(grid)))
+    column_win = any(
+        all(grid[j][i] == DRAWN for j in range(len(grid)))
+        for i in range(len(grid[0])))
+    return row_win or column_win
+
+
+def mark(grid, num):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == num:
+                grid[i][j] = DRAWN
+
+
 def gen_win_order(data):
     nums = [*map(int, data[0].split(","))]
     grids = {i: parse_grid(grid) for i, grid in enumerate(data[1:])}
     for num in nums:
         for grid_num, grid in [*grids.items()]:
-            # fill
-            for i in range(len(grid)):
-                for j in range(len(grid[0])):
-                    if grid[i][j] == num:
-                        grid[i][j] = DRAWN
-            # check winner
-            row_win = any(
-                all(x == DRAWN for x in grid[i]) for i in range(len(grid)))
-            column_win = any(
-                all(grid[j][i] == DRAWN for j in range(len(grid)))
-                for i in range(len(grid[0])))
-            if row_win or column_win:
+            mark(grid, num)
+            if is_won(grid):
                 yield unmarked_grid_sum(grid) * num
                 grids.pop(grid_num)
 
