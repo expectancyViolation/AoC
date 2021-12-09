8ce989cad991966146de75c4cbf24bc986c46ba2
matze <>
2021-12-10 00:27:36 +0100

    2021 day 9. bugfix

--- 2021/day09.py
+++ 2021/day09.py
@@ -1,5 +1,6 @@
-from collections import Counter
 from math import prod
+
+from disjoint_set import DisjointSet
 
 from util import *
 
@@ -9,30 +10,35 @@ YEAR = 2021
 
 @timing
 def get_risks(lines):
-    indices = {(int(x), int(y)): int(z)
-               for x, line in enumerate(lines) for y, z in enumerate(line)}
-    sink_target = {}
-    low_to_high = sorted(indices.items(), key=lambda tup: tup[1])
-    for i, ((x, y), z) in enumerate(low_to_high):
-        if z == 9:
-            break
-        for dx, dy in (-1, 0), (1, 0), (0, 1), (0, -1):
-            nb = (x + dx, y + dy)
-            if nb in sink_target:
-                sink_target[(x, y)] = sink_target[nb]
-                break
-        else:
-            sink_target[(x, y)] = (x, y)
-
-    sinks = {*sink_target.values()}
-    sizes = sorted(Counter(sink_target.values()).values())
-    p1 = sum(1 + indices[(x, y)] for x, y in sinks)
+    L = len(lines)
+    l = len(lines[0])
+    indices = [[int(z) for z in line] for line in lines]
+    ds = DisjointSet()
+    for x, line in enumerate(indices):
+        for y, z in enumerate(line):
+            if z == 9:
+                continue
+            for (dx, dy) in (1, 0), (-1, 0), (0, 1), (0, -1), (0, 0):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < L and 0 <= ny < l:
+                    val = indices[nx][ny]
+                    if val <= z:
+                        ds.union((x, y), (nx, ny))
+    # sinks = {(x, y) for _, (x, y) in ds}
+    sinks = {min((indices[x][y], (x,y)) for x,y in basin) for basin in ds.itersets()}
+    sinks = {pos for _, pos in sinks}
+    sizes = sorted(
+        [sum(1 for x in basin) for basin in ds.itersets()])
+    p1 = sum(1 + indices[x][y] for x, y in sinks)
     p2 = prod(sizes[-3:])
     return p1, p2
 
 
 if __name__ == "__main__":
-    raw_data = get_data(DAY, year=YEAR, raw=True)
+    raw_data = get_data(DAY, year=YEAR,
+                        raw=True)
+    # raw_data = get_data(DAY, filename="input/2021/09_bigboi.txt", year=YEAR,
+    #                     raw=True)
     data = raw_data.split("\n")
     part1, part2 = get_risks(data)
     print(part1)
