0cd0391d60f7131860139b5d588a6d374e54d2c7
matze <>
2021-12-10 01:04:07 +0100

    2021 day 9. numpy-edition

--- 2021/day09.py
+++ 2021/day09.py
@@ -1,6 +1,7 @@
 from math import prod
 
-from disjoint_set import DisjointSet
+import numpy as np
+from scipy.ndimage.measurements import label
 
 from util import *
 
@@ -9,38 +10,26 @@ YEAR = 2021
 
 
 @timing
-def get_risks(lines):
-    L = len(lines)
-    l = len(lines[0])
-    indices = [[int(z) for z in line] for line in lines]
-    ds = DisjointSet()
-    for x, line in enumerate(indices):
-        for y, z in enumerate(line):
-            if z == 9:
-                continue
-            for (dx, dy) in (1, 0), (-1, 0), (0, 1), (0, -1), (0, 0):
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < L and 0 <= ny < l:
-                    val = indices[nx][ny]
-                    if val <= z:
-                        ds.union((x, y), (nx, ny))
-    # sinks = {(x, y) for _, (x, y) in ds}
-    sinks = {min((indices[x][y], (x,y)) for x,y in basin) for basin in ds.itersets()}
-    sinks = {pos for _, pos in sinks}
-    sizes = sorted(
-        [sum(1 for x in basin) for basin in ds.itersets()])
-    p1 = sum(1 + indices[x][y] for x, y in sinks)
-    p2 = prod(sizes[-3:])
-    return p1, p2
+def solve(arr):
+    p = np.pad(arr, 1, constant_values=9)
+    low_points = (p < np.minimum.reduce([
+        np.roll(p, 1, axis=0),
+        np.roll(p, -1, axis=0),
+        np.roll(p, 1, axis=1),
+        np.roll(p, -1, axis=1)
+    ]))
+    risk_level = np.sum(p[low_points] + 1)
+    labeled, n = label(arr != 9)
+    unique, counts = np.unique(labeled, return_counts=True)
+    size_prod = prod(sorted(counts)[-4:-1])
+    return risk_level, size_prod
 
 
 if __name__ == "__main__":
-    raw_data = get_data(DAY, year=YEAR,
-                        raw=True)
-    # raw_data = get_data(DAY, filename="input/2021/09_bigboi.txt", year=YEAR,
-    #                     raw=True)
-    data = raw_data.split("\n")
-    part1, part2 = get_risks(data)
+    raw_data = get_data(DAY, year=YEAR, raw=True)
+    arr = np.array([[int(x) for x in line] for line in raw_data.split("\n")],
+                   dtype=int)
+    part1, part2 = solve(arr)
     print(part1)
     # submit(DAY, 1, part1,year=YEAR)
     print(part2)
