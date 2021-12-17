3aa72c6f20684fecf7e7cfc2d182ef89e92901fd
matze <>
2021-12-17 22:39:16 +0100

    2021 day 17 speedup (removed sympy)

--- 2021/day17.py
+++ 2021/day17.py
@@ -1,46 +1,52 @@
 import re
+from functools import lru_cache
 from itertools import product
+from math import sqrt, floor, ceil
 
-import sympy as sp
-from sympy import lambdify
+# import sympy as sp
+# from sympy import lambdify
 
 from util import *
 
 DAY = 17
 YEAR = 2021
 
+
 # unnecessarily complicated.
 # solve for time range spent in target for each coordinate and find intersection
 # sympy is slow...
-vx, vy, s, t = sp.symbols("vx,vy,s,t")
+# vx, vy, s, t = sp.symbols("vx,vy,s,t")
+#
+# py = sp.Sum(vy - s, (s, 0, t - 1)).doit()
+#
+# px = sp.Sum(vx - s, (s, 0, t - 1)).doit()
+#
+# b = sp.symbols("b")
+#
+# F = lambdify((vx, b), sp.solve(px - b, t)[0])
+# G = lambdify((vx, b), sp.solve(px - b, t)[1])
+# print(sp.solve(px - b, t)[0])
+# print(sp.solve(px - b, t)[1])
 
-py = sp.Sum(vy - s, (s, 0, t - 1)).doit()
 
-px = sp.Sum(vx - s, (s, 0, t - 1)).doit()
-
-b = sp.symbols("b")
-
-F = lambdify((vx, b), sp.solve(px - b, t)[0])
-G = lambdify((vx, b), sp.solve(px - b, t)[1])
+def F(vx, b):
+    return vx - sqrt(-8 * b + 4 * vx ** 2 + 4 * vx + 1) / 2 + 1 / 2
 
 
+def G(vx, b):
+    return vx + sqrt(-8 * b + 4 * vx ** 2 + 4 * vx + 1) / 2 + 1 / 2
+
+
+@lru_cache(maxsize=10 ** 10)
 def get_x_time_range(v_x, x_low, x_high):
-    p = px.subs(vx, v_x)
     max_end = v_x * (v_x + 1) // 2
-    if max_end >= x_low:
-        t_low = F(v_x, (x_low - .5))
-    else:
-        t_low = inf
-    if max_end >= x_high:
-        t_high = F(v_x, (x_high + .5))
-    else:
-        t_high = inf
+    t_low = ceil(F(v_x, (x_low - .5))) if max_end >= x_low else inf
+    t_high = F(v_x, (x_high + .5)) if max_end >= x_high else inf
+    return t_low, t_high
 
-    return sp.ceiling(t_low), sp.floor(t_high)
-
-
+@lru_cache(maxsize=10 ** 10)
 def get_y_time_range(v_y, y_low, y_high):
-    return sp.ceiling(G(v_y, y_high)), sp.floor(G(v_y, y_low))
+    return ceil(G(v_y, y_high)), floor(G(v_y, y_low))
 
 
 def get_is_ok(x_low, x_high, y_low, y_high):
@@ -58,12 +64,24 @@ def get_is_ok(x_low, x_high, y_low, y_high):
 def part2(x_low, x_high, y_low, y_high):
     okay = get_is_ok(x_low, x_high, y_low, y_high)
     res = 0
-    for x, y in product(range(1, x_high + 1), range(y_low - 2, -y_low + 2)):
-        if okay(x, y):
-            res += 1
+    x_s = []
+    for x in range(1, x_high + 1):
+        a, b = get_x_time_range(x, x_low, x_high)
+        if a <= b:
+            x_s.append(x)
+    y_s = []
+    for y in range(y_low - 2, -y_low + 2):
+        a, b = get_y_time_range(y, y_low, y_high)
+        if a <= b:
+            y_s.append(y)
+
+    for x, y in product(x_s, y_s):
+        res += okay(x, y)
     return res
 
 
+# DANGER: only works since there is a v_x for any t>0
+#   that gives a hit in the x coordinate
 def part1(_x_low, _x_high, y_low, _y_high):
     v_y = -y_low - 1
     return v_y * (v_y + 1) // 2
@@ -71,8 +89,8 @@ def part1(_x_low, _x_high, y_low, _y_high):
 
 if __name__ == "__main__":
     raw_data = get_data(DAY, year=YEAR, raw=True)
-    print(raw_data)
-    data = re.match(r"target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)", raw_data).groups()
+    data = re.match(r"target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)",
+                    raw_data).groups()
     data = [*map(int, data)]
     res = part1(*data)
     print(res)
