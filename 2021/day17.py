11268292aa328e2f225ec26e5a7db765a4f1aa83
matze <>
2021-12-18 02:33:54 +0100

    2021 day 17 even more speedup (O(d*log(d)) with d=x_max+y_max

--- 2021/day17.py
+++ 2021/day17.py
@@ -2,9 +2,12 @@ import re
 from functools import lru_cache
 from itertools import product
 from math import sqrt, floor, ceil
+from heapq import heappush, heappop
 
 # import sympy as sp
 # from sympy import lambdify
+import numpy as np
+
 
 from util import *
 
@@ -37,14 +40,13 @@ def G(vx, b):
     return vx + sqrt(-8 * b + 4 * vx ** 2 + 4 * vx + 1) / 2 + 1 / 2
 
 
-@lru_cache(maxsize=10 ** 10)
 def get_x_time_range(v_x, x_low, x_high):
     max_end = v_x * (v_x + 1) // 2
     t_low = ceil(F(v_x, (x_low - .5))) if max_end >= x_low else inf
-    t_high = F(v_x, (x_high + .5)) if max_end >= x_high else inf
+    t_high = floor(F(v_x, (x_high + .5))) if max_end >= x_high else inf
     return t_low, t_high
 
-@lru_cache(maxsize=10 ** 10)
+
 def get_y_time_range(v_y, y_low, y_high):
     return ceil(G(v_y, y_high)), floor(G(v_y, y_low))
 
@@ -62,21 +64,33 @@ def get_is_ok(x_low, x_high, y_low, y_high):
 
 @timing
 def part2(x_low, x_high, y_low, y_high):
-    okay = get_is_ok(x_low, x_high, y_low, y_high)
     res = 0
-    x_s = []
+    bounds = []
     for x in range(1, x_high + 1):
         a, b = get_x_time_range(x, x_low, x_high)
         if a <= b:
-            x_s.append(x)
-    y_s = []
+            bounds.append((a, 1, x))
+            bounds.append((b, 4, x))
+    # plt.show()
     for y in range(y_low - 2, -y_low + 2):
         a, b = get_y_time_range(y, y_low, y_high)
         if a <= b:
-            y_s.append(y)
+            bounds.append((a, 2, y))
+            bounds.append((b, 3, y))
+    x_open_intervals = set()
+    y_open_intervals = set()
+    for _val, _prio, v in sorted(bounds):
+        if _prio == 4:
+            x_open_intervals.remove(v)
+        elif _prio == 1:
+            res += len(y_open_intervals)
+            x_open_intervals.add(v)
+        elif _prio == 2:
+            res += len(x_open_intervals)
+            y_open_intervals.add(v)
+        elif _prio == 3:
+            y_open_intervals.remove(v)
 
-    for x, y in product(x_s, y_s):
-        res += okay(x, y)
     return res
 
 
@@ -89,6 +103,10 @@ def part1(_x_low, _x_high, y_low, _y_high):
 
 if __name__ == "__main__":
     raw_data = get_data(DAY, year=YEAR, raw=True)
+    # raw_data = "target area: x=20..30, y=-10..-5"
+    # raw_data="target area: x=22000..22045, y=-99960..-99956"
+    # raw_data='target area: x=282184..482382, y=-502273..-374688'
+    # raw_data = 'target area: x=6300659222..7198515181, y=-5865357542..-374274528'
     data = re.match(r"target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)",
                     raw_data).groups()
     data = [*map(int, data)]
