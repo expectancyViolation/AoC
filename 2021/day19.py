a3c815de8435914df2e1e1f1517a95eab040a31b
matze <>
2021-12-19 16:15:55 +0100

    2021 day 19 triangle based. quick

--- 2021/day19.py
+++ 2021/day19.py
@@ -4,11 +4,11 @@ from functools import lru_cache
 from itertools import combinations, permutations, product
 from math import prod
 from random import shuffle
+
 import matplotlib
-
 matplotlib.use("TkAgg")
-
 import matplotlib.pyplot as plt
+
 import numpy as np
 
 from util import *
@@ -21,18 +21,8 @@ def manhattan(v1, v2):
     return sum(abs(x - y) for x, y in zip(v1, v2))
 
 
-# use distance "signature" to speed up point matching
-# common points have to have same manhattan distance in both scans
-def all_distances(scanner):
-    distances = defaultdict(lambda: [])
-    for (i1, v1), (i2, v2) in combinations(enumerate(scanner), 2):
-        distances[manhattan(v1, v2)].append((i1, i2))
-    return dict(distances)
-
-
 def perm_inversions(perm):
-    return sum(perm[i] > perm[j]
-               for i, j in combinations(range(len(perm)), 2))
+    return sum(perm[i] > perm[j] for i, j in combinations(range(len(perm)), 2))
 
 
 def sign_inversions(signs):
@@ -41,8 +31,7 @@ def sign_inversions(signs):
 
 @lru_cache(maxsize=1)
 def get_valid_transforms(d=3):
-    return [(perm, signs)
-            for perm in permutations(range(d))
+    return [(perm, signs) for perm in permutations(range(d))
             for signs in product([-1, 1], repeat=d)
             if (perm_inversions(perm) + sign_inversions(signs)) % 2 == 0]
 
@@ -50,16 +39,23 @@ def get_valid_transforms(d=3):
 def transform(v, perm, signs):
     return [v[i] * s for i, s in zip(perm, signs)]
 
-@timing
-def find_mappings(scanners):
+
+def l2_sq(v1, v2):
+    return sum((x1 - x2) ** 2 for x1, x2 in zip(v1, v2))
+
+
+# this depends on the assumption that
+# any triangle of 3 points in the scans is unique
+# (i.e. its triple of side lengths)
+def find_correspondences(scanners):
     tris = defaultdict(lambda: {})
     for i, scanner in enumerate(scanners):
-        for (i1, p1), (i2, p2), (i3, p3) in combinations(enumerate(scanner), 3):
-            distances, indices = zip(*sorted(((l2_sq(p1, p2), i3),
-                                              (l2_sq(p2, p3), i1),
-                                              (l2_sq(p1, p3), i2))))
+        d_2 = [[l2_sq(p, q) for j, q in enumerate(scanner)]
+               for i, p in enumerate(scanner)]
+        for i1, i2, i3 in combinations(range(len(scanner)), 3):
+            sides = ((d_2[i1][i2], i3), (d_2[i2][i3], i1), (d_2[i1][i3], i2))
+            distances, indices = zip(*sorted(sides))
             tris[distances][i] = indices
-
     correspondences = defaultdict(lambda: [])
     for dist, lookups in tris.items():
         if len(lookups) <= 1:
@@ -74,16 +70,14 @@ def find_mappings(scanners):
         if len(mapping) >= 12:
             mappings[(i, j)] = mapping
             mappings[(j, i)] = inverse_mapping
+
     return mappings
 
 
-def find_overlap_matching(mappings, real_scan, scan2, i, j):
-    if (i, j) not in mappings:
-        return None
-    mapping = mappings[(i, j)]
+def find_overlap_matching(correspondences, real_scan, scan2):
     for trafo in get_valid_transforms():
         offset = None
-        for i1, i2 in mapping.items():
+        for i1, i2 in correspondences.items():
             transformed = transform(scan2[i2], *trafo)
             candidate_offset = tuple(
                 [x - y for x, y in zip(real_scan[i1], transformed)])
@@ -100,41 +94,36 @@ def find_overlap_matching(mappings, real_scan, scan2, i, j):
 
 @timing
 def solve(scanners):
-    mappings = find_mappings(scanners)
+    correspondences = find_correspondences(scanners)
     not_matched_indices = {i for i in range(1, len(scanners))}
-    distances = {
-        i: all_distances(scanner)
-        for i, scanner in enumerate(scanners)
-    }
     real_coords = {0: [x for x in scanners[0]]}
     frontier = {0}
-    offsets = [[0, 0, 0]]
+    offsets = {0: (0, 0, 0)}
     while frontier:
-        print(frontier)
-        print(not_matched_indices)
         nf = set()
         for i in frontier:
             coords = real_coords[i]
             for j in [*not_matched_indices]:
-                overlap = find_overlap_matching(mappings, coords, scanners[j],
-                                                i, j)
+                if (i, j) not in correspondences:
+                    continue
+                overlap = find_overlap_matching(correspondences[i, j], coords,
+                                                scanners[j])
                 if overlap:
                     best_match_coords, offset = overlap
                     not_matched_indices.remove(j)
                     nf.add(j)
                     real_coords[j] = best_match_coords
-                    offsets.append(offset)
+                    offsets[j] = offset
         frontier = nf
     m = 0
-    for v1, v2 in combinations(offsets, 2):
+    for v1, v2 in combinations(offsets.values(), 2):
         curr = sum(abs(x1 - x2) for x1, x2 in zip(v1, v2))
         m = max(m, curr)
-    scanner_positions = [(-x, -y, -z) for x, y, z in offsets]
-    all_coords = defaultdict(lambda: 0)
+    all_coords = defaultdict(lambda: [])
     for scanner_id, points in real_coords.items():
         for point in points:
-            all_coords[tuple(point)] += 1
-    return len(all_coords), m, scanner_positions, all_coords
+            all_coords[tuple(point)] += [scanner_id]
+    return len(all_coords), m, offsets, all_coords
 
 
 def parse_scanner(scanner):
@@ -147,16 +136,21 @@ def parse_scanner(scanner):
 def plot_result(scanner_positions, all_coords):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    X, Y, Z = zip(*scanner_positions)
+    X, Y, Z = zip(
+        *[x for i, x in enumerate(scanner_positions.values()) if i != 0])
     ax.scatter(X, Y, Z, marker="o")
-    X, Y, Z, W = zip(*[(*v, w) for v, w in all_coords.items() if w > 1])
+    x, y, z = scanner_positions[0]
+    ax.scatter(x, y, z, marker="o", color="red")
+    X, Y, Z, W = zip(
+        *[(*v, len(w)) for v, w in all_coords.items()])
     s = ax.scatter(X, Y, Z, c=W, marker="x")
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    for v, w in all_coords.items():
+        for i in w:
+            x, y, z = zip(*[v, scanner_positions[i]])
+            ax.plot(x, y, z, color=colors[i % len(colors)])
     plt.colorbar(s)
     plt.show()
-
-
-def l2_sq(v1, v2):
-    return sum((x1 - x2) ** 2 for x1, x2 in zip(v1, v2))
 
 
 if __name__ == "__main__":
@@ -169,8 +163,7 @@ if __name__ == "__main__":
     # print(C)
     print(part1, part2)
 
-    # plot_result(scanner_positions,all_coords)
-    print(time())
+    plot_result(scanner_positions, all_coords)
 
     # submit(DAY, 1, part1,year=YEAR)
     # submit(DAY, 2, part2,year=YEAR)
