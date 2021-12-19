d01f508d8f82c65a04207d8cf3ff92910822beb7
matze <>
2021-12-19 11:05:00 +0100

    2021 day 19 visualisation

--- 2021/day19.py
+++ 2021/day19.py
@@ -1,8 +1,14 @@
 import time
 from collections import Counter, defaultdict
+from functools import lru_cache
 from itertools import combinations, permutations, product
+from math import prod
 from random import shuffle
+import matplotlib
 
+matplotlib.use("TkAgg")
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 from util import *
@@ -15,114 +21,129 @@ def manhattan(v1, v2):
     return sum(abs(x - y) for x, y in zip(v1, v2))
 
 
+# use distance "signature" to speed up point matching
+# common points have to have same manhattan distance in both scans
 def all_distances(scanner):
     distances = defaultdict(lambda: [])
-    for (i1, v1), (i2, v2) in combinations(
-            enumerate(scanner), 2):
+    for (i1, v1), (i2, v2) in combinations(enumerate(scanner), 2):
         distances[manhattan(v1, v2)].append((i1, i2))
     return dict(distances)
 
 
+def perm_inversions(perm):
+    return sum(perm[i] > perm[j]
+               for i, j in combinations(range(len(perm)), 2))
+
+
+def sign_inversions(signs):
+    return sum(1 for x in signs if x == -1)
+
+
+@lru_cache(maxsize=1)
+def get_valid_transforms(d=3):
+    return [(perm, signs)
+            for perm in permutations(range(d))
+            for signs in product([-1, 1], repeat=d)
+            if (perm_inversions(perm) + sign_inversions(signs)) % 2 == 0]
+
+
+def transform(v, perm, signs):
+    return [v[i] * s for i, s in zip(perm, signs)]
+
+
 def find_overlap(d1, d2, scanner1, scanner2, score_limit=12):
-    reference_scanner = [tuple(x) for x in scanner1]
     reference_distances = d1
     reference_vectors = defaultdict(lambda: [])
     for d, pairs in reference_distances.items():
         for i, j in pairs:
             reference_vectors[d].append(tuple(scanner1[j] - scanner1[i]))
-    for perm in permutations([0, 1, 2]):
-        for signs in product([-1, 1], repeat=3):
-            match = 0
-            coords = [np.array([x[v] * s for v, s in zip(perm, signs)]) for
-                      x in
-                      scanner2]
-            offsets = defaultdict(lambda: 0)
-            for x in reference_distances:
-                if x not in d2:
-                    continue
-                for i, j in d2[x]:
-                    vec = coords[j] - coords[i]
-                    if vec[0] < 0:
-                        vec = -vec
-                    vec = tuple(vec)
-                    v = reference_vectors[x][0]
-                    for v in reference_vectors[x]:
-                        if vec == v:
-                            match += 1
-                            for a in i, j:
-                                for b in reference_distances[x][0]:
-                                    offsets[
-                                        tuple(coords[a] - reference_scanner[
-                                            b])] += 1
-            if match > 0:
-                cnt, transform = max((y, x) for x, y in offsets.items())
-                if cnt >= score_limit:
-                    best_match_offset = transform
-                    best_match_coords = [
-                        np.array([x - t for x, t in zip(X, transform)]) for X in
-                        coords]
-                    return best_match_coords, best_match_offset
-    return None, None
+    for perm, signs in get_valid_transforms():
+        match = False
+        coords = [np.array(transform(v, perm, signs)) for v in scanner2]
+        offset_counts = defaultdict(lambda: 0)
+        for x in reference_distances:
+            if x not in d2:
+                continue
+            for i, j in d2[x]:
+                vec = coords[j] - coords[i]
+                vec = tuple(vec)
+                vec_inv = tuple(vec)
+                for v in reference_vectors[x]:
+                    if vec == v or vec_inv == v:
+                        match = True
+                        for a in i, j:
+                            for b in reference_distances[x][0]:
+                                offset_counts[tuple(coords[a] -
+                                                    scanner1[b])] += 1
+        if match:
+            cnt, offset = max((y, x) for x, y in offset_counts.items())
+            if cnt >= score_limit:
+                best_match_offset = np.array(offset, dtype=int)
+                best_match_coords = [X - best_match_offset for X in coords]
+                return best_match_coords, best_match_offset
 
 
 @timing
-def part1(scanners):
+def solve(scanners):
     not_matched_indices = {i for i in range(1, len(scanners))}
-    distances = {i: all_distances(scanner) for i, scanner in
-                 enumerate(scanners)}
-    all_coords = {tuple(x) for x in scanners[0]}
-    # current_reference_points = scanners[0]
+    distances = {
+        i: all_distances(scanner)
+        for i, scanner in enumerate(scanners)
+    }
     real_coords = {0: [x for x in scanners[0]]}
     frontier = {0}
-    offsets = []
-    score_limit = 12
+    offsets = [[0, 0, 0]]
     while not_matched_indices:
+        print(frontier)
+        print(not_matched_indices)
         nf = set()
         for i in frontier:
             coords = real_coords[i]
             for j in [*not_matched_indices]:
-                best_match_coords, offset = find_overlap(distances[i],
-                                                         distances[j],
-                                                         coords, scanners[j],
-                                                         score_limit)
-                if best_match_coords:
-                    all_coords |= {tuple(x) for x in best_match_coords}
+                overlap = find_overlap(distances[i], distances[j], coords,
+                                       scanners[j])
+                if overlap:
+                    best_match_coords, offset = overlap
                     not_matched_indices.remove(j)
                     nf.add(j)
                     real_coords[j] = best_match_coords
                     offsets.append(offset)
         frontier = nf
-    print(all_coords)
-
     m = 0
     for v1, v2 in combinations(offsets, 2):
         curr = sum(abs(x1 - x2) for x1, x2 in zip(v1, v2))
         m = max(m, curr)
-    print(m)
-    return len(all_coords)
-
-
-def part2(data):
-    return None
+    scanner_positions = [(-x, -y, -z) for x, y, z in offsets]
+    all_coords = defaultdict(lambda: 0)
+    for scanner_id, points in real_coords.items():
+        for point in points:
+            all_coords[tuple(point)] += 1
+    return len(all_coords), m, scanner_positions, all_coords
 
 
 def parse_scatter(scanner):
-    # print(scanner)
-    return [np.array([*map(int, line.split(","))], dtype=int) for line in
-            scanner.split("\n")[1:]]
+    return [
+        np.array([*map(int, line.split(","))], dtype=int)
+        for line in scanner.split("\n")[1:]
+    ]
 
 
 if __name__ == "__main__":
     raw_data = get_data(DAY, year=YEAR, raw=True)
-    # raw_data = datata
     results = []
     times = []
     scanners = [*map(parse_scatter, raw_data.split("\n\n"))]
-    print(len(scanners))
-    shuffle(scanners)
-    res = part1(scanners)
-    print(res)
-    # submit(DAY, 1, res,year=YEAR)
-    # res = part2(data)
-    # print(res)
-    # submit(DAY, 2, res,year=YEAR)
+    part1, part2, scanner_positions, all_coords = solve(scanners)
+    C = Counter(all_coords.values())
+    print(C)
+    print(part1, part2)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    X, Y, Z = zip(*scanner_positions)
+    ax.scatter(X, Y, Z, marker="o")
+    X, Y, Z, W = zip(*[(*v, w) for v, w in all_coords.items() if w>1])
+    s=ax.scatter(X, Y, Z,c=W, marker="x")
+    plt.colorbar(s)
+    plt.show()
+    # submit(DAY, 1, part1,year=YEAR)
+    # submit(DAY, 2, part2,year=YEAR)
