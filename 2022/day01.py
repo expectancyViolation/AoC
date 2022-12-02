2a061426b44020126345ee18d64e449fd38d00f6
MTh <->
2022-12-02 09:59:54 +0100

    2022 day 02

--- 2022/day01.py
+++ 2022/day01.py
@@ -4,25 +4,27 @@ DAY = 1
 YEAR = 2022
 
 
-def part1(data):
+def get_group_values(data):
     groups = data.split("\n\n")
-    group_values = [sum(int(x) for x in group.split()) for group in groups]
+    return [sum(int(x) for x in group.split()) for group in groups]
+
+
+def part1(data):
+    group_values = get_group_values(data)
     return max(group_values)
 
 
 def part2(data):
-    groups = data.split("\n\n")
-    group_values = [sum(int(x) for x in group.split()) for group in groups]
+    group_values = get_group_values(data)
     group_values.sort()
     return sum(group_values[-3:])
 
 
 if __name__ == "__main__":
     data = get_data(DAY, year=YEAR, raw=True)
-    print(data)
     res = part1(data)
     print(res)
     # submit(DAY, 1, res,year=YEAR)
     res = part2(data)
     print(res)
-    submit(DAY, 2, res, year=YEAR)
+    # submit(DAY, 2, res, year=YEAR)
