dd2cbeec1903b69cf5f8d738488eafe8ddfa0f2f
matze <>
2022-12-13 20:49:23 +0100

    2022 day 13 cleaned up

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,4 +1,5 @@
 import functools
+from math import prod
 
 from util import *
 
@@ -6,77 +7,46 @@ DAY = 13
 YEAR = 2022
 
 
-def compare(l1, l2):
-    if isinstance(l1, int):
-        l1 = [l1]
-    if isinstance(l2, int):
-        l2 = [l2]
-    if len(l1) == 0 and len(l2) == 0:
-        return 0
-    if len(l1) == 0:
-        return -1
+def compare_int(i1, i2):
+    return (i2 < i1) - (i1 < i2)
 
-    if len(l2) == 0:
-        return 1
 
-    left = l1[0]
-    right = l2[0]
-    if isinstance(left, int) and isinstance(right, int):
-        if left < right:
-            return -1
-        if left > right:
-            return 1
-        return compare(l1[1:], l2[1:])
-    if isinstance(left, int):
-        left = [left]
-    if isinstance(right, int):
-        right = [right]
-    for x, y in zip(left, right):
-        c = compare(x, y)
-        if c:
+def compare(p1, p2):
+    i1, i2 = (isinstance(x, int) for x in (p1, p2))
+    if i1 and i2:
+        return compare_int(p1, p2)
+    if i1:
+        p1 = [p1]
+    if i2:
+        p2 = [p2]
+    for a, b in zip(p1, p2):
+        if c := compare(a, b):
             return c
-    l_left = len(left)
-    l_right = len(right)
-    if l_left < l_right:
-        return -1
-    if l_right < l_left:
-        return 1
-    return compare(l1[1:], l2[1:])
+    return compare_int(len(p1), len(p2))
 
 
 def part1(data):
     total = 0
     for i, pair in enumerate(data.split("\n\n")):
-        p1, p2 = pair.split("\n")
-        p1 = eval(p1)
-        p2 = eval(p2)
-        res = compare(p1, p2)
-        assert res != 0
-        if res == -1:
-            total += (i + 1)
+        p1, p2 = map(eval, pair.split("\n"))
+        total += (i + 1) * (compare(p1, p2) == -1)
     return total
 
 
 def part2(data):
-    values = [eval(p) for pair in data.split("\n\n") for p in pair.split("\n")]
+    values = [eval(p) for p in data.split("\n") if p]
     delimiters = [[[2]], [[6]]]
     values += delimiters
     values = sorted(values, key=functools.cmp_to_key(compare))
-    res = 1
-    for i, val in enumerate(values, start=1):
-        print(val, val in delimiters)
-        if val in delimiters:
-            res *= i
-    return res
+    return prod(i for i, val in enumerate(values, start=1)
+                if val in delimiters)
 
 
 if __name__ == "__main__":
-    # data = get_data(DAY, year=YEAR, raw=True, filename="input/2022/13_test.txt")
     data = get_data(DAY, year=YEAR, raw=True)
-    # print(data)
     res = part1(data)
     print(res)
     # submit(DAY, 1, res, year=YEAR)
     res = part2(data)
     print(res)
-    submit(DAY, 2, res,year=YEAR)
+    # submit(DAY, 2, res, year=YEAR)
