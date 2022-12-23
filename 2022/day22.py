10beba1f58f9c24cac5e95ce6128036952467099
MTh <->
2022-12-23 02:45:46 +0100

    2022 day 22 small cleanup

--- 2022/day22.py
+++ 2022/day22.py
@@ -12,11 +12,12 @@ YEAR = 2022
 facings = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
-def part1(map, path):
-    L = len(map)
-    l = len(map[0])
+@timing
+def part1(map_, path):
+    L = len(map_)
+    l = len(map_[0])
     x = 0
-    y = map[x].index(".")
+    y = map_[x].index(".")
     facing = 0
     for step in path:
         cnt = int(step[:-1])
@@ -26,15 +27,14 @@ def part1(map, path):
             nx, ny = x, y
             while True:
                 nx, ny = (nx + fx) % L, (ny + fy) % l
-                if map[nx][ny] != " ":
+                if map_[nx][ny] != " ":
                     break
-            if map[nx][ny] == ".":
+            if map_[nx][ny] == ".":
                 x, y = nx, ny
             else:
                 break
         facing = (facing + turn) % 4
 
-    print(x, y, facing)
     return 1000 * (x + 1) + 4 * (y + 1) + facing
 
 
@@ -47,68 +47,77 @@ def cross(v1, v2):
     return y1 * z2 - z1 * y2, z1 * x2 - x1 * z2, x1 * y2 - x2 * y1
 
 
-def part2(map, block_size=50):
-    three_d_map = {}
+def add(v1, v2):
+    x1, y1, z1 = v1
+    x2, y2, z2 = v2
+    return x1 + x2, y1 + y2, z1 + z2
+
+
+def scale(a, v):
+    x, y, z = v
+    return a * x, a * y, a * z
+
+
+# floodfill map_ keeping track of the 3d cube positions and axis orientations
+#  during crossings of "folds" (cube-edges; position only dependent on cube size)
+# 3d point coordinates are centered on the faces of the cube (start position is at origin)
+# 3d surface normals point "inwards" into the cube
+def floodfill_fold_3d(map_, start_pos, cube_size=50):
+    three_d_map_ = {}
     x_dir = {}
     y_dir = {}
-    L = len(map)
-    l = len(map[0])
-    x = 0
-    y = map[x].index(".")
+    L = len(map_)
+    l = len(map_[0])
+    x, y = start_pos
     frontier = {(x, y)}
-    three_d_map[(x, y)] = (0, 0, 0)
+    three_d_map_[(x, y)] = (0, 0, 0)
     distances = {(x, y): 0}
     x_dir[(x, y)] = (1, 0, 0)
     y_dir[(x, y)] = (0, 1, 0)
     while frontier:
         new_frontier = set()
         for x, y in frontier:
-            tx, ty, tz = three_d_map[(x, y)]
+            t_pos = three_d_map_[(x, y)]
             for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
                 nx, ny = x + dx, y + dy
-                if (nx, ny) in three_d_map:
+                if (nx, ny) in three_d_map_:
                     continue
                 if not ((0 <= nx < L) and (0 <= ny < l)):
                     continue
-                if map[nx][ny] != ".":
+                if map_[nx][ny] != ".":
                     continue
                 if dx:
-                    # crossed fold
-                    xdx, xdy, xdz = x_dir[(x, y)]
-                    if {x % block_size, nx % block_size} == {0, block_size - 1}:
-                        ntx, nty, ntz = new_x_dir = cross((dx * xdx, dx * xdy, dx * xdz), y_dir[(x, y)])
-                        assert sum(abs(a) for a in (ntx, nty, ntz)) == 1
+                    x_step = x_dir[(x, y)]
+                    if {x % cube_size, nx % cube_size} == {0, cube_size - 1}:
+                        # crossed fold
+                        x_dir[(nx, ny)] = cross(scale(dx, x_step), y_dir[(x, y)])
+                        x_step = add(x_step, x_dir[(nx, ny)])
                     else:
-                        ntx, nty, ntz = 0, 0, 0
-                        new_x_dir = x_dir[(x, y)]
-                    three_d_map[(nx, ny)] = tx + dx * (ntx + xdx), ty + dx * (nty + xdy), tz + dx * (ntz + xdz)
-                    x_dir[(nx, ny)] = new_x_dir
+                        x_dir[(nx, ny)] = x_dir[(x, y)]
+                    three_d_map_[(nx, ny)] = add(t_pos, scale(dx, x_step))
                     y_dir[(nx, ny)] = y_dir[(x, y)]
 
                 elif dy:
-                    # crossed fold
-                    ydx, ydy, ydz = y_dir[(x, y)]
-                    if {y % block_size, ny % block_size} == {0, block_size - 1}:
-                        ntx, nty, ntz = new_y_dir = cross(x_dir[(x, y)], (dy * ydx, dy * ydy, dy * ydz))
-                        print(ntx, nty, ntz)
-                        assert sum(abs(a) for a in (ntx, nty, ntz)) == 1
+                    y_step = y_dir[(x, y)]
+                    if {y % cube_size, ny % cube_size} == {0, cube_size - 1}:
+                        # crossed fold
+                        y_dir[(nx, ny)] = cross(x_dir[(x, y)], scale(dy, y_step))
+                        y_step = add(y_step, y_dir[(nx, ny)])
                     else:
-                        ntx, nty, ntz = 0, 0, 0
-                        new_y_dir = y_dir[(x, y)]
-
-                    three_d_map[(nx, ny)] = tx + dy * (ntx + ydx), ty + dy * (nty + ydy), tz + dy * (ntz + ydz)
-                    y_dir[(nx, ny)] = new_y_dir
+                        y_dir[(nx, ny)] = y_dir[(x, y)]
+                    three_d_map_[(nx, ny)] = add(t_pos, scale(dy, y_step))
                     x_dir[(nx, ny)] = x_dir[(x, y)]
                 new_frontier.add((nx, ny))
                 distances[(nx, ny)] = distances[(x, y)] + 1
         frontier = new_frontier
-    print(three_d_map)
-    reverse_three_d_map = {p3: p2 for p2, p3 in three_d_map.items()}
 
-    z_dir = {(x, y): cross(x_dir[(x, y)], y_dir[(x, y)]) for x, y in three_d_map}
+    return three_d_map_, x_dir, y_dir
 
-    p = [(x, y) for x, y in three_d_map.keys() if distances[(x, y)] < 10**10]
-    points = np.array([three_d_map[x] for x in p])
+
+def draw_cube(three_d_map_, x_dir, y_dir):
+    z_dir = {(x, y): cross(x_dir[(x, y)], y_dir[(x, y)]) for x, y in three_d_map_}
+    p = [(x, y) for x, y in three_d_map_.keys()]
+    points = np.array([three_d_map_[x] for x in p])
     nx = np.array([x_dir[x] for x in p])
     ny = np.array([y_dir[x] for x in p])
     nz = np.array([z_dir[x] for x in p])
@@ -120,32 +129,40 @@ def part2(map, block_size=50):
     ax.quiver(points[:, 0], points[:, 1], points[:, 2], .3 * nz[:, 0], .3 * nz[:, 1], .3 * nz[:, 2], color="blue")
     plt.show()
 
-    x = 0
-    y = 0
-    z = 0
+
+@timing
+def part2(map_, path, cube_size=50):
+    start_pos = (0, map_[0].index("."))
+    three_d_map_, x_dir, y_dir = floodfill_fold_3d(map_, start_pos, cube_size)
+
+    z_dir = {(x, y): cross(x_dir[(x, y)], y_dir[(x, y)]) for x, y in three_d_map_}
+    reverse_three_d_map_ = {p3: p2 for p2, p3 in three_d_map_.items()}
+
+    curr_pos = (0, 0, 0)
     facing = (0, 1, 0)
     for step in path:
         cnt = int(step[:-1])
         turn = step[-1]
         for _ in range(int(cnt)):
-            dx, dy, dz = facing
-            nx, ny, nz = x + dx, y + dy, z + dz
-            if (nx, ny, nz) in reverse_three_d_map:
-                x, y, z = nx, ny, nz
+            new_pos = add(curr_pos, facing)
+            if new_pos in reverse_three_d_map_:
+                curr_pos = new_pos
             else:
-                new_facing = z_dir[reverse_three_d_map[(x, y, z)]]
-                dx, dy, dz = new_facing
-                nx, ny, nz = nx + dx, ny + dy, nz + dz
-                if (nx, ny, nz) in reverse_three_d_map:
-                    x, y, z = nx, ny, nz
+                # try crossing a fold (by additionally stepping "inwards" in surface normal direction)
+                new_facing = z_dir[reverse_three_d_map_[curr_pos]]
+                new_pos = add(new_pos, new_facing)
+                if new_pos in reverse_three_d_map_:
+                    curr_pos = new_pos
                     facing = new_facing
         if turn == "R":
-            facing = cross(facing, z_dir[reverse_three_d_map[(x, y, z)]])
+            facing = cross(facing, z_dir[reverse_three_d_map_[curr_pos]])
         elif turn == "L":
             dx, dy, dz = facing
-            facing = cross((-dx, -dy, -dz), z_dir[reverse_three_d_map[(x, y, z)]])
+            facing = cross((-dx, -dy, -dz), z_dir[reverse_three_d_map_[curr_pos]])
 
-    sol_x, sol_y = reverse_three_d_map[(x, y, z)]
+    sol_x, sol_y = reverse_three_d_map_[curr_pos]
+    draw_cube(three_d_map_,x_dir,y_dir)
+    # generate 3d facing lookup by rotating the 3d y-direction (corresponding to 2d (0,1)) clockwise
     facing_lookup = []
     q = y_dir[(sol_x, sol_y)]
     for _ in range(4):
@@ -155,19 +172,21 @@ def part2(map, block_size=50):
     return 1000 * (sol_x + 1) + 4 * (sol_y + 1) + sol_face
 
 
+def parse_input(data):
+    *map_, my_path = data
+    map_ = [line.rstrip() for line in map_]
+    l_fill = max(len(line) for line in map_)
+    map_ = [line.ljust(l_fill) for line in map_]
+    path = (my_path + "N").replace("L", "L,").replace("R", "R,").split(",")
+    return map_, path
+
+
 if __name__ == "__main__":
-    *map, path = get_data(DAY, year=YEAR)
-    # *map, path = get_data(DAY, year=YEAR, filename="input/2022/22_test.txt")
-    map = [line.rstrip() for line in map]
-    l_fill = max(len(line) for line in map)
-    map = [line.ljust(l_fill) for line in map]
-    for line in map:
-        print(line)
-    path = (path + "N").replace("L", "L,").replace("R", "R,").split(",")
-    print(path)
-    # res = part1(map, path)
-    # print(res)
+    data = get_data(DAY, year=YEAR,filename="input/2022/22_test.txt")
+    my_map, my_path = parse_input(data)
+    res = part1(my_map, my_path)
+    print(res)
     # submit(DAY, 1, res, year=YEAR)
-    res = part2(map, 50)
+    res = part2(my_map, my_path, cube_size=4)
     print(res)
     # submit(DAY, 2, res,year=YEAR)
